93e5c229839c5afe89b5595841a989cae97ae983
zainab <zeinab@kuwaitnet.dev>
2023-04-15 16:38:29 +0300

    feat: add game, Team serializers

--- games/core/serializers.py
+++ games/core/serializers.py
@@ -6,6 +6,8 @@ from django.core.validators import FileExtensionValidator
 from djvue.serializers import FileUploadSerializer
 from rest_framework.reverse import reverse_lazy
 from .helper_functions import import_teams, import_games
+from .models import Game, Team
+
 
 class CSVUploadSerializer(FileUploadSerializer):
     """ Allows only CSV files to be uploaded """
@@ -24,3 +26,41 @@ class HandleUploadSerializer(serializers.Serializer):
         import_teams(file)
         import_games(file)
         return super(HandleUploadSerializer, self).save(**kwargs)
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    """ serialize team objects"""
+
+    class Meta:
+        model = Team
+        Fields = ('pk', 'name',)
+        read_only_fields = ('pk',)
+
+
+class GameSerializer(serializers.ModelSerializer):
+    """serialize game objects"""
+    team1 = TeamSerializer()
+    team2 = TeamSerializer()
+
+    class Meta:
+        model = Game
+        fields = (
+            "pk",
+            "team1",
+            "team2",
+            "team1_score",
+            "team2_score"
+        )
+
+    def update(self, instance, validated_data):
+        """ update game data, in case of new Team , it will be created then added to game"""
+        # make sure to add only team.name in the front
+        team1_name = validated_data.pop("team1", None)
+        team2_name = validated_data.pop("team2", None)
+        obj1, created = Team.objects.get_or_create(name=team1_name)
+        obj2, created = Team.objects.get_or_create(name=team2_name)
+        instance = super(GameSerializer, self).update(instance, validated_data)
+        instance.team1 = obj1
+        instance.team2 = obj2
+        instance.save()
+        return instance
