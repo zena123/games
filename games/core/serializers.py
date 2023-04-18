d51c888e344c39050afe2871bb7eae72dc87abe4
zainab <zeinab@kuwaitnet.dev>
2023-04-18 18:16:36 +0300

    fix: fix upload url

--- games/core/serializers.py
+++ games/core/serializers.py
@@ -1,44 +1,48 @@
-from djvue.fields import FileField
-from rest_framework import serializers
-
 from django.core.validators import FileExtensionValidator
-
+from djvue.fields import FileField
 from djvue.serializers import FileUploadSerializer
+from rest_framework import serializers
 from rest_framework.reverse import reverse_lazy
-from .helper_functions import import_teams, import_games
+
+from .helper_functions import import_games, import_teams
 from .models import Game, Team
 
 
 class CSVUploadSerializer(FileUploadSerializer):
-    """ Allows only CSV files to be uploaded """
+    """Allows only CSV files to be uploaded"""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["file"].validators.append(FileExtensionValidator(allowed_extensions=['csv']))
+        self.fields["file"].validators.append(
+            FileExtensionValidator(allowed_extensions=["csv"])
+        )
 
 
 class HandleUploadSerializer(serializers.Serializer):
-    """ get uploaded file and call function to import data"""
-    csv_file = FileField(style={"upload_url": reverse_lazy("csv_upload")})
+    csv_file = FileField(style={"upload_url": reverse_lazy("core:csv_upload")})
 
     def create(self, validated_data):
-        file = validated_data.pop('csv_file', None)
-        import_teams(file['path'])
-        import_games(file['path'])
+        file = validated_data.pop("csv_file", None)
+        import_teams(file["path"])
+        import_games(file["path"])
         return file
 
 
 class TeamSerializer(serializers.ModelSerializer):
-    """ serialize team objects"""
+    pk = serializers.IntegerField()
+    name = (
+        serializers.CharField()
+    )  # disable the validation since it's nested and unique will cause issues
 
     class Meta:
         model = Team
-        fields = ('pk', 'name',)
-        read_only_fields = ('pk',)
+        fields = (
+            "pk",
+            "name",
+        )
 
 
 class GameSerializer(serializers.ModelSerializer):
-    """serialize game objects"""
     team1 = TeamSerializer()
     team2 = TeamSerializer()
 
@@ -49,20 +53,16 @@ class GameSerializer(serializers.ModelSerializer):
             "team1",
             "team1_score",
             "team2",
-            "team2_score"
+            "team2_score",
         )
 
-    # def update(self, instance, validated_data):
-    #     """ update game data, in case of new Team , it will be created then added to game"""
-    #     # make sure to add only team.name in the front
-    #     print("helllooooo")
-    #     team1_name = validated_data.pop("team1", None)
-    #     team2_name = validated_data.pop("team2", None)
-    #     # obj1, created = Team.objects.get_or_create(name=team1_name)
-    #     # obj2, created = Team.objects.get_or_create(name=team2_name)
-    #     instance = super(GameSerializer, self).update(instance, validated_data)
-    #     # instance.team1 = obj1
-    #     # instance.team2 = obj2
-    #     print(instance)
-    #     instance.save()
-    #     return instance
+    def update(self, instance, validated_data):
+        """update game data, in case of new Team """
+        team1 = validated_data.pop("team1", None)
+        team2 = validated_data.pop("team2", None)
+        if team1 and team1.get("name"):
+            Team.objects.filter(pk=team1["pk"]).update(name=team1["name"])
+        if team2 and team2.get("name"):
+            Team.objects.filter(pk=team2["pk"]).update(name=team2["name"])
+        instance = super(GameSerializer, self).update(instance, validated_data)
+        return instance
